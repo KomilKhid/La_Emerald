c4da94ba98f56dccc6d8f8acbda5efb8d19ead2c
Komil <115709729+KomilKhid@users.noreply.github.com>
2024-08-26 14:06:07 +0500

    Update SpecialMenu.jsx

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -7,7 +7,7 @@ import './SpecialMenu.css';
 const SpecialMenu = () => (
   <div className="app__specialMenu flex__center section__padding" id="menu">
     <div className="app__specialMenu-title">
-      <SubHeading title="Menu that fits your palatte" />
+      <SubHeading title="Discover a Menu Crafted for Your Exquisite Taste" />
       <h1 className="headtext__cormorant">Today&apos;s Special</h1>
     </div>
 
